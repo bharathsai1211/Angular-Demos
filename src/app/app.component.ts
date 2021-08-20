f9be9fcac374c5a121d56ccbfce3424728301890
bharatsai1211 <bharatsai1211@gamil.com>
2021-08-21 03:51:34 +0530

    auto completion using Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo';
+export class AppComponent implements OnInit {
+  control = new FormControl();
+  testForm!: FormGroup;
+  streets: string[][] = [['Champs-Élysées', '1'], ['Lombard Street', '2'], ['Abbey Road', '3'], ['Fifth Avenue', '4']];
+  filteredStreets: Observable<string[]> | undefined;
+  stre: string[] = [];
+
+  constructor(private fb: FormBuilder) {
+  }
+
+
+  onKeypressEvent(event: any) {
+
+    console.log(event.target.value);
+
+
+  }
+  ngOnInit() {
+
+
+    this.testForm = this.fb.group({
+      control: new FormControl(''),
+
+    });
+
+
+
+    this.filteredStreets = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+
+
+  }
+
+  private _filter(value: string): string[] {
+    if (value.length < 2) {
+      return [];
+    }
+    const filterValue = this._normalizeValue(value);
+    this.stre = [];
+    for (let i = 0; i < this.streets.length; i++) {
+      this.stre[i] = this.streets[i][0] + ", " + this.streets[i][1];
+    }
+    return this.stre.filter(street => this._normalizeValue(street).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+
+  value(data: any): void {
+    console.log(data);
+    console.log(this.control.value);
+  }
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
